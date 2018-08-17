cfec128f45e2035ea2e1540df24c702b3a15f48e
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-17 12:11:09 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,75 @@
+class Dog 
+  attr_accessor :name, :breed, :id 
+  
+  def initialize(args)
+    @name = args[:name]
+    @breed = args[:breed]
+  end
+  
+  def self.create_table
+    sql = <<-SQL 
+      CREATE TABLE dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      );
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL 
+      DROP TABLE dogs;
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL 
+        INSERT INTO dogs(name, breed) VALUES (?, ?);
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      self
+    end
+  end
+  
+  def self.create(args)
+    dog = self.new(args)
+    dog.save
+    dog
+  end
+  
+  def self.find_by_id(id)
+    sql = <<-SQL 
+      SELECT * FROM dogs WHERE id = ?
+    SQL
+    res = DB[:conn].execute(sql, id)
+    if !res.empty?
+      dog = self.new({
+        :name => res[0][1],
+        :breed => res[0][2]
+      })
+      dog.id = res[0][0]
+      dog
+    end
+  end
+  
+  def self.find_or_create_by(args)
+    sql = <<-SQL 
+      SELECT * FROM dogs WHERE name = ?
+    SQL
+    res = DB[:conn].execute(sql, args[:name], args[:breed])
+    if res.empty?
+      self.create(args)
+    else
+      if res[0][2] == args[:breed]
+      else
+      end
+      
+    end
+  end
+end
